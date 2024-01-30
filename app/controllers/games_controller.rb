61b331d76907f034be8b3099db8a11ba2d3d7dd5
Cristian Manolache <mlcristian@outlook.com>
2024-01-30 23:03:19 +0000

    added the logic of the game , fully functional

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,8 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
@@ -5,8 +10,22 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
     @answer = params[:answer]
+
+    if valid_word?(@answer)
+      @message = "Congratulations! #{@answer} is a valid English word!"
+    else
+      @message = "Sorry but #{@answer} does not seem to be a valid English word..."
+    end
   end
 
+private
+
+  def valid_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    result = JSON.parse(response)
+    result['found']
+  end
 end
